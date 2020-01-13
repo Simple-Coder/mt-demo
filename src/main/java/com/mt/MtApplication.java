ad080eea032bc5b14ef5afaa479e5283055a17d4
xiedong <xiedong@rmitec.cn>
2020-01-13 13:53:10 +0800

    init

---
+++ src/main/java/com/mt/MtApplication.java
@@ -0,0 +1,19 @@
+package com.mt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: MtApplication
+ * @Description:
+ * @Author: xiedong
+ * @Date: 2020/1/13 13:40
+ */
+@SpringBootApplication
+@Slf4j
+public class MtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MtApplication.class);
+    }
+}
---
+++ src/main/java/com/mt/service/ICustInfoService.java
@@ -0,0 +1,10 @@
+package com.mt.service;
+
+/**
+ * @Interface: ICustInfoService
+ * @Description:
+ * @Author: xiedong
+ * @Date: 2020/1/13 13:45
+ */
+public interface ICustInfoService {
+}
